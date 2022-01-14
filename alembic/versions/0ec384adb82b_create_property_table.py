ba6cfc81b971d99d02411812b131fa44f84123b4
KielRodriguez <kr.rdz.20@gmail.com>
2022-01-14 02:34:30 -0600

    creacion modelo property

---
+++ alembic/versions/0ec384adb82b_create_property_table.py
@@ -0,0 +1,32 @@
+"""create property table
+
+Revision ID: 0ec384adb82b
+Revises: 
+Create Date: 2022-01-14 00:35:12.663230
+
+"""
+from alembic import op
+from sqlalchemy import Column
+import sqlalchemy.dialects.postgresql as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0ec384adb82b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'properties',
+        Column('id', sa.UUID, primary_key=True),
+        Column('bed', sa.SMALLINT, nullable=False),
+        Column('bad', sa.FLOAT, nullable=False),
+        Column('parking_spots', sa.SMALLINT, nullable=False),
+        Column('size', sa.FLOAT, nullable=False),
+        Column('price', sa.MONEY, nullable=False)
+    )
+
+def downgrade():
+    op.drop_table('properties')
